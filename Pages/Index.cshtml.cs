e5e3f1bf4b8370af94d4cc06120de8b5de17ed34
José Medeiros <nicruo@gmail.com>
2024-05-21 12:37:43 +0100

    Added more cards;

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -13,17 +13,21 @@ namespace Decker.Pages
         public List<PromptCard> Cards { get; set; }
 
 
-
         public IndexModel(ILogger<IndexModel> logger, CardService cardService)
         {
             _logger = logger;
             this.cardService = cardService;
         }
 
-        public void OnGet()
+        public IActionResult OnGet(string? card)
         {
+            if(!string.IsNullOrWhiteSpace(card))
+            {
+                return RedirectToPage("/Card", new { id = card });
+            }
             _logger.Log(LogLevel.Information, "Index page visited");
             Cards = cardService.GetCards();
+            return Page();
         }
     }
 }
